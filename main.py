65010ddd01d3f19b712f269b6e58ee9d01afe232
小马哥 <1612823478@qq.com>
2024-08-14 17:55:38 +0800

    修复签到

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-
+# 签到、抽奖
 import sys
 import requests
 # 签到
-URL = "https://api.juejin.cn/growth_api/v1/check_in"
+URL = "https://api.juejin.cn/growth_api/v1/check_in?aid=2606"
 # 首次抽奖
 LOTTERY = 'https://api.juejin.cn/growth_api/v1/lottery/draw'
 api_list = [URL, LOTTERY]
 herder = {
     'referer': 'https://juejin.cn/',
     'origin': 'https://juejin.cn',
-    'content-type': 'application/json',
+    'content-type': 'application/json; charset=utf-8',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
     'cookie': sys.argv[1]
 }
@@ -22,7 +22,7 @@ def access_check_in():
     """
     for api in api_list:
         resp = requests.post(url=api, headers=herder)
-        print(resp.json())
+        print(resp.text)
 
 
 if __name__ == '__main__':
